d21943e3612670685efbc6b32fc97ef057e62323
d3rdone <19626442+d3rdOne@users.noreply.github.com>
2024-08-04 23:13:24 +0800

    Added Hero, Skill and Contact Component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SkillComponent } from './components/skill/skill.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent, SkillComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
