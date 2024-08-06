6f400945ca47719d60808f7a9798486b0472d47d
d3rdone <19626442+d3rdOne@users.noreply.github.com>
2024-08-06 21:14:41 +0800

    Integrated Skills, About, and Experience component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { SkillComponent } from './components/skill/skill.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, SkillComponent],
+  imports: [HeroComponent, AboutMeComponent, SkillComponent, ExperienceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
