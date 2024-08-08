fccda09445de658165e4d268e62b44698fe7d878
d3rdone <19626442+d3rdOne@users.noreply.github.com>
2024-08-08 23:03:46 +0800

    Fix for build issue

--- src/app/features/home/components/hero/hero.component.ts
+++ src/app/features/home/components/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
-
+  @Output() scrollDownClicked:any = new EventEmitter();
 }
