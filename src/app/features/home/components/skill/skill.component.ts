2adb6c9299b2d2222c34b82367ece580d96bac43
d3rdone <19626442+d3rdOne@users.noreply.github.com>
2024-08-08 16:12:18 +0800

    Updated skills data

--- src/app/features/home/components/skill/skill.component.ts
+++ src/app/features/home/components/skill/skill.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { TECHNOLOGIES } from '../../../../../skils';
 
 @Component({
   selector: 'app-skill',
@@ -12,52 +13,5 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class SkillComponent {
 
   showColored: boolean = false;
-  tools: any[] = [{
-    label: 'Angular',
-    monoImg: '',
-    imgUrl: 'angular.svg'
-  }, {
-    label: 'React',
-    monoImg: '',
-    imgUrl: 'react.svg'
-  },
-  {
-    label: 'HTML',
-    monoImg: '',
-    imgUrl: 'html.svg'
-  },
-  {
-    label: 'Javascript',
-    monoImg: '',
-    imgUrl: 'javascript.svg'
-  },
-  // {
-  //   label: 'CSS',
-  //   monoImg: '',
-  //   imgUrl: 'css-3.svg'
-  // },
-  {
-    label: 'SCSS',
-    monoImg: '',
-    imgUrl: 'scss.svg'
-  }, {
-    label: 'Git',
-    monoImg: '',
-    imgUrl: "git-icon.svg"
-  },
-  { label: 'Tailwind',
-    monoImg: '',
-    imgUrl: 'tailwindcss.svg'
-  }, {
-    label: 'NGXS',
-    monoImg: '',
-    imgUrl: 'ngxs.png'
-  }, {
-    label: 'Single-Spa',
-    monoImg: '',
-    imgUrl: 'single-spa.svg'
-  }]
-  constructor(private sanitizer: DomSanitizer) {
-    this.tools = this.tools.map(tool => ({...tool, imgUrl: 'tools/'+tool.imgUrl}))
-  }
+  tools: any[] =TECHNOLOGIES;
 }
---
+++ src/skils.ts
@@ -0,0 +1,42 @@
+import { Technology } from "./app/core/models/technology";
+
+export const TECHNOLOGIES: Technology[] = [{
+  label: 'Angular',
+  monoImg: '',
+  imgUrl: 'tools/angular.svg'
+}, {
+  label: 'React',
+  monoImg: '',
+  imgUrl: 'tools/react.svg'
+},
+{
+  label: 'HTML',
+  monoImg: '',
+  imgUrl: 'tools/html.svg'
+},
+{
+  label: 'Javascript',
+  monoImg: '',
+  imgUrl: 'tools/javascript.svg'
+},
+{
+  label: 'SCSS',
+  monoImg: '',
+  imgUrl: 'tools/scss.svg'
+}, {
+  label: 'Git',
+  monoImg: '',
+  imgUrl: "tools/git-icon.svg"
+},
+{ label: 'Tailwind',
+  monoImg: '',
+  imgUrl: 'tools/tailwindcss.svg'
+}, {
+  label: 'RXJS',
+  monoImg: '',
+  imgUrl: 'tools/rxjs.svg'
+}, {
+  label: 'Single-Spa',
+  monoImg: '',
+  imgUrl: 'tools/single-spa.svg'
+}]
